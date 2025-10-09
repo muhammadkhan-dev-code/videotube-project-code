96723c43469dcab885d935bde7da10484fdc159e
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-10-09 17:00:43 +0500

    Refactor user routes: organize imports, fix route definitions, and add missing endpoints for password change, account update, and user channel profile

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -18,10 +29,28 @@ router.route("/register").post(
   registerUser
 );
 
- router.route("/login").post(loginUser);
+router.route("/login").post(loginUser);
 
- router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 
- router.route("/refresh-token").post( refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .router("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/channel/:username".get(verifyJWT, getUserChannelProfile));
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
