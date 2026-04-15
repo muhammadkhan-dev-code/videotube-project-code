9a74155e768cf5f2f28458bcd5d54b96dc5cb1b6
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2026-04-15 23:44:30 +0500

    some code updated

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
 
@@ -11,7 +11,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
       req.header("Authorization")?.replace("Bearer ", "");
   
     if (!token) {
-      throw new ApiError("unauthorized Request ", 401);
+      throw new ApiError(401, "Unauthorized request");
     }
     // verify token
 
@@ -22,12 +22,24 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     );
   
     if (!user) {
-      throw new ApiError("Invalid Access Token ", 401);
+      throw new ApiError(401, "Invalid access token");
     }
     req.user = user;
     next();
   
   } catch (error) {
-   throw new ApiError(`unauthorized Request invalid Access Token ${error.message} `, 401); 
+    if (error instanceof ApiError) {
+      throw error;
+    }
+
+    if (error instanceof jwt.TokenExpiredError) {
+      throw new ApiError(401, "Access token expired");
+    }
+
+    if (error instanceof jwt.JsonWebTokenError) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    throw new ApiError(401, "Unauthorized request");
   }
 });
